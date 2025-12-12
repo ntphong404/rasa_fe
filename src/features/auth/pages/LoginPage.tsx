9db75efbdf16b20d25fd689e5e0707197e9ac870
Nguyễn Tuấn Khải <105345374+Khaidzvippro@users.noreply.github.com>
2025-12-12 22:59:16 +0700

    fix logo

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -109,7 +109,7 @@ export function LoginPage({
         <CardContent>
           <div className="mx-auto mb-0 w-32 h-32 p-0 items-center">
             <AspectRatio ratio={1}>
-              <img src="/logo.png" alt="Logo" className="rounded-md object-cover" />
+              <img src="/logo2.png" alt="Logo" className="rounded-md object-cover" />
             </AspectRatio>
           </div>
 
